2e93a0d89498674095c0f046f894d38c10edb553
siddh34 <ssiddharthsutar@gmail.com>
2024-08-01 14:38:19 +0530

    fixed bugs

--- src/components/news.jsx
+++ src/components/news.jsx
@@ -1,47 +1,31 @@
 import React, { useEffect, useState } from "react";
 import NewsItem from "./newsItem";
 import Spinner from "./spinner";
-import PropTypes from "prop-types";
 import InfiniteScroll from "react-infinite-scroll-component";
 
 const News = (props) => {
   const [article, setArticle] = useState([]);
-  const [loading, setLoading] = useState(false);
   const [page, setPage] = useState(1);
-  const [progress, setProgress] = useState(0);
   const [totalResults, setTotalResults] = useState(0);
   const [fetchedResults, setFetchedResults] = useState(0);
   // document.title = `NewsMonkey - ${props.category.charAt(0).toUpperCase() + props.category.slice(1)}`;
 
   const updateNews = async () => {
-    setProgress(5);
     const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&apiKey=${process.env.REACT_APP_NEWS_API_KEY}&category=${props.category}&page=${page}&pageSize=${props.pageSize}`;
-    setLoading(true);
-
-    setProgress(10);
-
     const data = await fetch(url);
-
-    setProgress(50);
-
     const parsedData = await data.json();
-    setProgress(70);
-
     setArticle(parsedData.articles);
     setTotalResults(parsedData.totalResults);
     setFetchedResults(parsedData.articles.length);
-    setLoading(false);
-
-    setProgress(100);
   };
 
   const fetchMoreData = async () => {
+    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&apiKey=${process.env.REACT_APP_NEWS_API_KEY}&category=${props.category}&page=${page + 1}&pageSize=${props.pageSize}`;
     setPage(page + 1);
-    const prevArticleLength = article.length;
-    await updateNews();
-    if (article.length === prevArticleLength) {
-      setFetchedResults(totalResults);
-    }
+    const data = await fetch(url);
+    const parsedData = await data.json();
+    setArticle(article.concat(parsedData.articles));
+    setFetchedResults(article.length + parsedData.articles.length);
   };
 
   useEffect(() => {
@@ -50,7 +34,10 @@ const News = (props) => {
 
   return (
     <>
-      <h1 className="my-3 mx-3">Top Headlines</h1>
+      <h1 className="my-3 mx-3">
+        {props.category.charAt(0).toUpperCase() + props.category.slice(1)} - Top
+        Headlines
+      </h1>
       <div className="Container my-3 mx-3">
         <InfiniteScroll
           dataLength={article.length}
